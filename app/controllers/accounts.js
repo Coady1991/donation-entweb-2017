3fe93b80b04310573c569ea1eadc0dc0593b80ab
Niall Coady <niall_coady7@hotmail.com>
2017-10-03 11:41:17 +0100

    Validation for Registering

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const User = require('../models/user');
+const Joi = require('joi');
 
 exports.main = {
   auth: false,
@@ -53,6 +54,23 @@ exports.logout = {
 
 exports.register = {
   auth: false,
+  validate: {
+    options: {
+      abortEarly: false,
+    },
+    payload: {
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+    failAction: function (request, reply, source, error) {
+      reply.view('signup', {
+        title: 'Sign up error',
+        errors: error.data.details,
+      }).code(400);
+    },
+  },
   handler: function (request, reply) {
     const user = new User(request.payload);
     user.save().then(newUser => {
