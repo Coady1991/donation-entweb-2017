799c12f8be4cf90f8476d14a2eec51f5e5db2786
Niall Coady <niall_coady7@hotmail.com>
2017-12-29 15:37:41 +0000

    Testing android

--- app/models/db.js
+++ app/models/db.js
@@ -5,28 +5,28 @@ mongoose.Promise = global.Promise;
 
 //let dbURI = 'mongodb://localhost/donation';
 let dbURI = 'mongodb://donationuser:donationuser@ds113915.mlab.com:13915/donation';
-if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGOLAB_URI;
-}
+// if (process.env.NODE_ENV === 'production') {
+//   dbURI = process.env.MONGOLAB_URI;
+// }
 
 mongoose.connect(dbURI);
 
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose connected to ' + dbURI);
-  if (process.env.NODE_ENV != 'production') {
-    var seeder = require('mongoose-seeder');
-    const data = require('./data.json');
-    const Donation = require('./donation');
-    const User = require('./user');
-    const Candidate = require('./candidate.js');
-    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
-      console.log('preloading Test Data');
-      console.log(dbData);
-    }).catch(err => {
-      console.log(error);
-    });
-  }
-});
+// mongoose.connection.on('connected', function () {
+//   console.log('Mongoose connected to ' + dbURI);
+//   if (process.env.NODE_ENV != 'production') {
+//     var seeder = require('mongoose-seeder');
+//     const data = require('./data.json');
+//     const Donation = require('./donation');
+//     const User = require('./user');
+//     const Candidate = require('./candidate.js');
+//     seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+//       console.log('preloading Test Data');
+//       console.log(dbData);
+//     }).catch(err => {
+//       console.log(error);
+//     });
+//   }
+// });
 
 mongoose.connection.on('error', function (err) {
   console.log('Mongoose connection error: ' + err);
